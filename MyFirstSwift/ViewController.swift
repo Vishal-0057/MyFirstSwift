21f3d72e60d86023989e090f102924e1f2e4aea0
Vishal Gupta <vishalcse.0057@gmail.com>
2015-08-22 11:16:17 +0530

    init commit

--- MyFirstSwift/ViewController.swift
+++ MyFirstSwift/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var titleLabel : UILabel?
+    @IBOutlet weak var tableView  : UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.titleLabel?.text = "prateek"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "TableCell")
+        
+        cell.textLabel?.text = "prateek"
+        var c  = indexPath.row
+        cell.contentView.backgroundColor = UIColor(red: 1, green: 121, blue: 1, alpha: 1)
+        return cell;
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60;
+    }
 }
 
